6bbc80de4b773832826154cfe2ae63e91bf7a3cd
sjlouji <sjlouji10@gmail.com>
2021-04-22 22:34:29 +0530

    permissions added

--- app/route/admin/blackList.js
+++ app/route/admin/blackList.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const blackList  = require('../../controller/admin/BlackListController');
 const passport = require('passport');
-const { insert, view } = require('../../middleware/permissions/BlackListPermission');
+const { insert, view, edit, remove } = require('../../middleware/permissions/BlackListPermission');
 
 router.post('/insert', passport.authenticate('jwt',{ session: false }), insert ,blackList.blackListName);
 router.get('/', passport.authenticate('jwt',{ session: false }), view, blackList.listNames);
-router.put('/update', passport.authenticate('jwt',{ session: false }), blackList.updateNames);
-router.delete('/delete', passport.authenticate('jwt',{ session: false }), blackList.deleteName);
+router.put('/update', passport.authenticate('jwt',{ session: false }), edit, blackList.updateNames);
+router.delete('/delete', passport.authenticate('jwt',{ session: false }), remove, blackList.deleteName);
 
 module.exports = router;
